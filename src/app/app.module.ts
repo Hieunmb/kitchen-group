50b9a669efe46ae112df1fffcb6d06e886fb5288
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2022-11-16 15:36:40 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {NavComponent} from "./nav/nav.component";
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes = [
+  {path:'header',component:HeaderComponent},
+  {path:'nav',component:NavComponent},
+  {path:'footer',component:FooterComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, FooterComponent, NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes), HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
