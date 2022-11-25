6ecede0f1a22763928d2710309d3d3b960ab2fb1
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2022-11-25 15:20:57 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,27 @@ import {FooterComponent} from "./footer/footer.component";
 import {NavComponent} from "./nav/nav.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
+import {HomeComponent} from "./home/home.component";
+import {AboutusComponent} from "./aboutus/aboutus.component";
+import {ShopComponent} from "./shop/shop.component";
+import {PageComponent} from "./page/page.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const appRoutes: Routes = [
   {path:'header',component:HeaderComponent},
   {path:'nav',component:NavComponent},
   {path:'footer',component:FooterComponent},
+  {path:'home',component:HomeComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'page',component:PageComponent},
+  {path:'contact',component:ContactComponent},
 ];
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, NavComponent
+    AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent,
+    AboutusComponent, ShopComponent, PageComponent, ContactComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(appRoutes), HttpClientModule
