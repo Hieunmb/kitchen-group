03a36ba76d34fda9bd54dccc0f4938db5daf015e
Hieunmb <zhuongdae@gmail.com>
2022-11-25 15:42:05 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,25 @@ import {NavComponent} from "./nav/nav.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {HomeComponent} from "./home/home.component";
-import {AboutusComponent} from "./aboutus/aboutus.component";
-import {ShopComponent} from "./shop/shop.component";
-import {PageComponent} from "./page/page.component";
+import {AboutUsComponent} from "./aboutus/aboutus.component";
 import {ContactComponent} from "./contact/contact.component";
+import {PageComponent} from "./page/page.component";
+import {ShopComponent} from "./shop/shop.component";
 
 const appRoutes: Routes = [
+  {path:'aboutus',component:AboutUsComponent},
+  {path:'home',component:HomeComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'page',component:PageComponent},
+  {path:'shop',component:ShopComponent},
   {path:'header',component:HeaderComponent},
   {path:'nav',component:NavComponent},
   {path:'footer',component:FooterComponent},
-  {path:'home',component:HomeComponent},
-  {path:'aboutus',component:AboutusComponent},
-  {path:'shop',component:ShopComponent},
-  {path:'page',component:PageComponent},
-  {path:'contact',component:ContactComponent},
 ];
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent,
-    AboutusComponent, ShopComponent, PageComponent, ContactComponent
+    AppComponent, HeaderComponent, FooterComponent, NavComponent,HomeComponent,AboutUsComponent,HeaderComponent,ContactComponent,PageComponent,ShopComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(appRoutes), HttpClientModule
