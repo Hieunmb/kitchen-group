54de9daa4c65dde881b56534b335625037756722
tomnn1608 <nxt16082004@gmail.com>
2022-11-25 15:41:49 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,20 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {NavComponent} from "./nav/nav.component";
+import {SigninComponent} from "./signin/signin.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-import {HomeComponent} from "./home/home.component";
-import {AboutusComponent} from "./aboutus/aboutus.component";
-import {ShopComponent} from "./shop/shop.component";
-import {PageComponent} from "./page/page.component";
-import {ContactComponent} from "./contact/contact.component";
 
 const appRoutes: Routes = [
   {path:'header',component:HeaderComponent},
   {path:'nav',component:NavComponent},
   {path:'footer',component:FooterComponent},
-  {path:'home',component:HomeComponent},
-  {path:'aboutus',component:AboutusComponent},
-  {path:'shop',component:ShopComponent},
-  {path:'page',component:PageComponent},
-  {path:'contact',component:ContactComponent},
+  {path:'signin',component:SigninComponent},
 ];
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent,
-    AboutusComponent, ShopComponent, PageComponent, ContactComponent
+    AppComponent, HeaderComponent, FooterComponent, NavComponent ,SigninComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(appRoutes), HttpClientModule
