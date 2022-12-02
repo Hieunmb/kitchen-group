617b93576b470ed9d6baf00779260e07af16a96c
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2022-12-03 03:56:46 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,12 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import {CartComponent} from "./cart/cart.component";
 import {ProductComponent} from "./product/product.component";
 import {MywishlistComponent} from "./mywishlist/mywishlist.component";
-import {WishlistComponent} from "./wishlist/wishlist.component";
 import {DeliveryComponent} from "./delivery/delivery.component";
 import {TermsandconditionComponent} from "./termsandcondition/termsandcondition.component";
+import {SearchComponent} from "./search/search.component";
 
 const appRoutes: Routes = [
+  {path:'',component:HomeComponent},
   {path:'header',component:HeaderComponent},
   {path:'nav',component:NavComponent},
   {path:'footer',component:FooterComponent},
@@ -36,16 +37,16 @@ const appRoutes: Routes = [
   {path:'cart',component:CartComponent},
   {path:'home/product',component:ProductComponent},
   {path:'mywishlist',component:MywishlistComponent},
-  {path:'wishlist',component:WishlistComponent},
   {path:'delivery',component:DeliveryComponent},
   {path:'page/termsandcondition',component:TermsandconditionComponent},
+  {path:'search',component:SearchComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent,
     AboutusComponent, ShopComponent, PageComponent, ContactComponent , RegisterComponent ,SigninComponent ,
-    CartComponent,ProductComponent,MywishlistComponent,WishlistComponent, DeliveryComponent, TermsandconditionComponent
+    CartComponent,ProductComponent,MywishlistComponent, DeliveryComponent, TermsandconditionComponent, SearchComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(appRoutes), HttpClientModule, CarouselModule
