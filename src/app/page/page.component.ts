5812d1d7530475debe582ccb6eee5b26cbb033c1
Hieunmb <zhuongdae@gmail.com>
2022-12-07 17:37:10 +0700

    first commit

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page',
@@ -6,5 +7,58 @@ import {Component} from "@angular/core";
   styleUrls: ['./page.component.css']
 })
 export class PageComponent{
+  product: any[] = [
+    { id: 1, brandname:'ga'},
+  ];cookware: any[] = [
+    { id: 1, name:'ga'},
+  ];refrigeration: any[] = [
+    { id: 1, name:'ga'},
+  ];appliances: any[] = [
+    { id: 1, name:'ga'},
+  ];foodstorage: any[] = [
+    { id: 1, name:'ga'},
+  ];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(){
+    this.demobrandname();
+    this.democookware();
+    this.demofoodstorage();
+    this.demoappliances();
+    this.demorefrigeration();
+  }
+  demobrandname() {
+    const url = 'http://localhost:5001/brand'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.product = data;
+      })
+  }democookware() {
+    const url = 'http://localhost:5001/cookware'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.cookware = data;
+      })
+  }demorefrigeration() {
+    const url = 'http://localhost:5001/refrigeration'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.refrigeration = data;
+      })
+  }demoappliances() {
+    const url = 'http://localhost:5001/appliances'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.appliances = data;
+      })
+  }demofoodstorage() {
+    const url = 'http://localhost:5001/foodstorage'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.foodstorage = data;
+      })
+  }
+
 
 }
